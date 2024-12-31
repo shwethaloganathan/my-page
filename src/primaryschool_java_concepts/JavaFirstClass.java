0b1cb01811e7a47a36ee2dc52f1dc63ddc304ae2
sakiv <sakiv@Buddy>
2024-12-30 20:50:36 -0500

    my first commit

---
+++ src/primaryschool_java_concepts/JavaFirstClass.java
@@ -0,0 +1,33 @@
+package primaryschool_java_concepts;
+
+public class JavaFirstClass {
+
+	public static void add() {
+		System.out.println("iam variable");
+		
+		
+	}	
+	public static void sub() {
+		System.out.println("global and local variable");
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// Here QA will write the code
+		System.out.println("welcome to school java");
+		System.out.println("lets learn java basics");
+		System.out.println("download java server and eclipse");
+		System.out.println("create project and name it with lower case from eclipse file menu");
+		System.out.println("right click on project and inside it create package and name it with lower case");
+		System.out.println("right click on package and create class and name it with camel case");
+		System.out.println("write the script as per the scenario");
+		add();
+		sub();
+		
+		
+	
+		
+		}
+
+}
---
+++ src/primaryschool_java_concepts/Variables_class.java
@@ -0,0 +1,51 @@
+package primaryschool_java_concepts;
+
+public class Variables_class {
+	//Global variable
+	//local variable
+	// int, short, long, byte
+	
+	
+	public static void add() {
+		System.out.println("ADD METHOD");
+		int a = 20;
+		int b = 30;
+		System.out.println(a + b);
+	}
+	
+	public static void sub() {
+		System.out.println("SUB METHOD");
+		int a = 80;
+		int b = 70;
+		System.out.println(a - b);
+	}
+	
+	public static void lesser_greater() {
+		System.out.println("LnG METHOD");
+		int a = 50;
+		int b = 90;
+		System.out.println(a > b);
+		System.out.println(a < b);
+		
+	}
+	
+	public static void dif_size_num() {
+         byte num;
+    	
+    	num = 127;
+    	System.out.println(num);
+
+		
+			}
+	
+	
+	public static void main(String[] args) {
+	 add();
+	 sub();
+	 lesser_greater();
+	 dif_size_num();
+	 
+
+	}
+
+}
